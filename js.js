28e3936822380e2c8f2d69f172b68800dd67252f
EatsMyLife <cgt0006000075653@gmail.com>
2016-12-30 14:54:19 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,10 +1,10 @@
-$(document).ready(function () {  
-    
+$(document).ready(function () {
+        
     $.ajax({             
         url: "http://api.openweathermap.org/data/2.5/weather",
         method: "GET",
         data: {
-            q: "taichung",
+            q: $("#city").text() + "," + $("#country").text(),
             appid: "061f24cf3cde2f60644a8240302983f2",
             units: "metric"
         },
@@ -13,8 +13,14 @@ $(document).ready(function () {
             console.log(respond.weather[0].main);
             console.log(respond.weather[0].description);
             console.log(respond.weather[0].icon);
-            console.log(respond.main.temp);            
-        }
+            console.log(respond.main.temp);
+            $("#weather-main").text(respond.weather[0].main);
+            $("#temperature").text(respond.main.temp);
+            $("#weather-icon").attr("src", "http://openweathermap.org/img/w/" + respond.weather[0].icon + ".png");
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown) { 
+        alert("Status: " + textStatus); alert("Error: " + errorThrown); 
+    } 
     });    
     
 });
