67c785029d731bbc2e0b4e2538e3d91a1fe23e76
EatsMyLife <cgt0006000075653@gmail.com>
2016-12-30 15:10:33 +0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
         
     $.ajax({             
-        url: "http://api.openweathermap.org/data/2.5/weather",
+        url: "http://api.openweathermap.org/data/2.5/weather?callback=haha",
         method: "GET",
         data: {
             q: $("#city").text() + "," + $("#country").text(),
             appid: "061f24cf3cde2f60644a8240302983f2",
             units: "metric"
         },
-        dataType: "json",
+        dataType: "jsonp",
+        jsonpCallback:"haha",
         success: function (respond) {
             console.log(respond.weather[0].main);
             console.log(respond.weather[0].description);
